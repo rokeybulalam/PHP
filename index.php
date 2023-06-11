d6e4b66ff084907e80faeebc6b0ee96943d3a9e9
rokeybulalam <rokeybulalam1122@gmail.com>
2023-06-11 11:40:50 +0600

    second rdition

--- index.php
+++ index.php
@@ -15,10 +15,14 @@ $array = [
     ]
    ];
 
+   $message  = 'Github connected';
   
    echo "my name is {$array['name']} {$array['age']}";
    echo "i live at {$array['Address']['village']},{$array['Address']['post']}, {$array['Address']['upazilla']},{$array['Address']['district']}";
    echo "my phone nmbr {$array['mobile_number']['country_prefix']}, {$array['mobile_number']['national_number']}";
+
+
+   echo $message;
 
 
 
