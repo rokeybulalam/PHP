5b84ee0a9977903340403bfde271b737a2886fab
rokeybulalam <rokeybulalam1122@gmail.com>
2023-06-11 11:36:09 +0600

    initila code

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+$array = [
+   'name' => 'Rokeybul',
+   'age' => '23',
+   'Address' => [
+      'village' => 'Kazlapara',
+      'post' => 'sukhanpukur',
+      'upazilla' => 'Gabtali',
+      'district' => 'Bogura'
+   ],
+    'mobile_number' => [
+      'country_prefix' => '+088',
+      'national_number' => '1980164177'
+    ]
+   ];
+
+  
+   echo "my name is {$array['name']} {$array['age']}";
+   echo "i live at {$array['Address']['village']},{$array['Address']['post']}, {$array['Address']['upazilla']},{$array['Address']['district']}";
+   echo "my phone nmbr {$array['mobile_number']['country_prefix']}, {$array['mobile_number']['national_number']}";
+
+
+
+
+
+
